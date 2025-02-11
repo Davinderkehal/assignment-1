66b3774a488d05e1a2692fffe11734f01bb0a2f7
DavinderKehal <kehalds@gmail.com>
2025-02-10 22:45:35 -0500

    some more comments

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,10 @@
-import Navbar from "./components/navbar";
+// Import the Navbar component (ensure the path is correct)
+
+import Navbar from " ./components/navbar";
 
 export default function About() {
   return (
-    <div style={{ textAlign: "center", padding: "20px" }}>
+    <div style={{ textAlign: "center", padding: "20px" }}> 
       <Navbar />
       <h1>About Page</h1>
       <p>Welcome to my web application!</p>
