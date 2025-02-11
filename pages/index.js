49dff9f8d49d2070fda20045ec376ff5a7a2433d
DavinderKehal <kehalds@gmail.com>
2025-02-10 22:50:44 -0500

    some more comments

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,28 @@
-import Navbar from "./components/navbar";
-import Counter from "./components/counter";
-import Toggle from "./components/toggle";
-import Form from "./components/form";
+// Import necessary components (ensure correct paths)
+import Navbar from " ./components/navbar";
+import Counter from " ./components/counter";
+import Toggle from " ./components/toggle";
+import Form from " ./components/form";
 
 export default function Home() {
   return (
     <div style={{ textAlign: "center", padding: "20px" }}>
+      {/* Include the navigation bar */}
       <Navbar />
-      <h1>This is to My Next.js App for Assignment-1</h1>
+
+      {/* Main heading of the application */}
+      <h1>This is My Next.js App for Assignment-1</h1>
+
+      {/* Brief description of the project */}
       <p>This application demonstrates key React concepts like state management and event handling.</p>
+
+      {/* Counter component to demonstrate state changes */}
       <Counter />
+
+      {/* Toggle component to show conditional rendering */}
       <Toggle />
+
+      {/* Form component to handle user input */}
       <Form />
     </div>
   );
